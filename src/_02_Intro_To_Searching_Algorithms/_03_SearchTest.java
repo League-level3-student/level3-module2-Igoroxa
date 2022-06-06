a5dac551d410e9386858957a1e4e51e9ab3abe19
league <league@imac-34.attlocal.net>
2022-06-06 16:58:05 -0700

    new assignement

--- src/_02_Intro_To_Searching_Algorithms/_03_SearchTest.java
+++ src/_02_Intro_To_Searching_Algorithms/_03_SearchTest.java
@@ -13,13 +13,22 @@ class _03_SearchTest {
     @Test
     public void testLinearSearch() {
         // 1. Use the assertEquals() method to test your linear search method
-        assertEquals(false, true);
+        String[] array = {"integer", "round", "value"};
+    	assertEquals(2, _01_LinearSearch.linearSearch(array, "value"));
+    	assertEquals(1, _01_LinearSearch.linearSearch(array, "round"));
+    	assertEquals(-1, _01_LinearSearch.linearSearch(array, "multiplication"));
+   
+    	
+        
     }
 
     @Test
     public void testBinarySearch() {
         // 2. Use the assertEquals() method to test your binary search method
         //    remember that the array must be sorted
-        assertEquals(false, true);
+    	int[] array = {0, 1, 2, 3, 4};
+        assertEquals(3, _02_BinarySearch.binarySearch(array, 1, 4, 3));
+        assertEquals(2, _02_BinarySearch.binarySearch(array, 0, 4, 2));
+        assertEquals(-1, _02_BinarySearch.binarySearch(array, 0, 4, 6));
     }
 }
--- src/_03_More_Algorithms/Algorithms.java
+++ src/_03_More_Algorithms/Algorithms.java
@@ -8,9 +8,27 @@ import java.util.List;
 public class Algorithms {
     
     public static String multiply(int num1, int num2) {
-        return null;
+     int lastM = num1*num2; 
+    	return num1 + " x " + num2 + " = " + lastM;
     }
-    
+    static boolean isPrime(int input) {
+		for (int i = 2; i < input-1; i++) {
+			
+			if (input % i == 0) {
+				System.out.println(i);
+				return false;
+			}
+		}
+
+    	return true;
+    	
+    }
+    static boolean isSquare(int input) {
+		for (int i = 0; i < array.length; i++) {
+			
+		}
+    	
+    }
     public static int findBrokenEgg(List<String> eggs) {
         return 0;
     }
